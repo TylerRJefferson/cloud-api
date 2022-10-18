05bb3e40efe96e4a2671c00fb6664d4227ad0d28
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-18 16:46:43 -0400

    database and api both working and deployed to the CLOUD

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 import functions from "firebase-functions";
 import express from "express";
+import { createUser, getAllUsers } from "./src/users.js";
 
 const app = express();
+app.use(express.json());
 
+app.get('/users', getAllUsers)
+app.post('/users', createUser)
 app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
 
 export const api = functions.https.onRequest(app)
